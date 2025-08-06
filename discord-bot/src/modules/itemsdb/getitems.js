7e1cfe2b55989a8240a98331c7dce9c3731e25ec
edlost113 <pattis78@gmail.com>
2025-08-06 15:00:52 -0400

    fixed scancommand logic, returns the list of items

--- discord-bot/src/modules/itemsdb/getitems.js
+++ discord-bot/src/modules/itemsdb/getitems.js
@@ -1,7 +1,7 @@
 const { globalHandler } = require('../handler.js');
 
 const { DynamoDBClient } = require( "@aws-sdk/client-dynamodb");
-const { DynamoDBDocumentClient, GetCommand } = require( "@aws-sdk/lib-dynamodb");
+const { DynamoDBDocumentClient, ScanCommand } = require( "@aws-sdk/lib-dynamodb");
 
 
 exports.data = {
@@ -22,20 +22,40 @@ const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 
 const action = async (body) => {  
+  let response = {}
+  try {
+    const params = {
+          TableName: "Items",
+          FilterExpression: "contains(#attr, :value)", // Filter to check
+          ExpressionAttributeNames: {
+            "#attr": "name", // Replace with your attribute name
+          },
+          ExpressionAttributeValues: {
+            ":value": body.data.options[0]['value'], // Replace with the value to search for
+          },
+    };
 
-    const command = new GetCommand({
-        TableName: "Items",
-        Key: {
-        name: "Amulet",
-        },
+    const command = new ScanCommand(params);
+    const responseDB = await client.send(command);
+
+    console.log(responseDB);
+    
+    let output = "Looked for items with the name: "+body.data.options[0]['value']+"\n"+
+    "👀  found "+ responseDB.Count +" items with similar name\n"+
+    "👀  out of "+ responseDB.ScannedCount +" total items\n\n";
+    const scanResults = [];
+    responseDB.Items.forEach((item) => scanResults.push(item));
+    scanResults.forEach((item) => {
+      output =  output + item.name+"\n\t\tPrice: "+item.priceAverage+"gp\n";
     });
-
-    const dynamoResponse = await docClient.send(command);
-    console.log(dynamoResponse);
-
-    let output = "⚔️ "+ body.data.options[0]['value'];
     response = {
-    "content": output,
+      "content": output,
+    }
+  } catch (error) {
+    console.error("Error scanning table:", error);
+    response = {
+      "content": "Error?",
+    }
   }
   return response
 }
